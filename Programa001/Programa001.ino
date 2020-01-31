577dc08c176a42a71a0ba5f856372e83ed306d7d
maxbesora2020 <besoram@gmail.com>
2020-01-31 12:18:16 +0100

    Update Programa001.ino

--- Programa001/Programa001.ino
+++ Programa001/Programa001.ino
@@ -8,156 +8,129 @@
 //********** Includes *************************************************************
 
 //********** Variables ************************************************************
-const int Vermell1 = 2;          // donar nom al pin 5 de l’Arduino
-const int Groc1= 3;          // donar nom al pin 6 de l’Arduino
-const int Verd1= 4;  
-const int Vermell2 = 5;          // donar nom al pin 7 de l’Arduino
-const int Groc2 =6 ;          // donar nom al pin 8 de l’Arduino
-const int Verd2= 7;          // donar nom al pin 9 de l’Arduino
+const int A = 1;          // donar nom al pin 5 de l’Arduino
+const int B= 2;          // donar nom al pin 6 de l’Arduino
+const int C= 3;  
+const int D= 4;          // donar nom al pin 7 de l’Arduino
+const int E= 5;          // donar nom al pin 8 de l’Arduino
+const int F= 6;          // donar nom al pin 9 de l’Arduino
+const int G= 7;          // donar nom al pin 9 de l’Arduino
 
 unsigned int temps=1000; 
 
 //********** Setup ****************************************************************
 void setup()
 {
-  pinMode(Vermell1, OUTPUT);     
-  pinMode(Groc1, OUTPUT);     
-  pinMode(Verd1, OUTPUT);     
-  pinMode(Vermell2, OUTPUT);     
-  pinMode(Groc2, OUTPUT);     
-  pinMode(Verd2, OUTPUT);    
+  pinMode(A, OUTPUT);     
+  pinMode(B, OUTPUT);     
+  pinMode(C, OUTPUT);     
+  pinMode(D, OUTPUT);     
+  pinMode(E, OUTPUT);     
+  pinMode(F, OUTPUT);
+  pinMode(G, OUTPUT);     
 
 }
 
 void loop()
 {
-  digitalWrite(Vermell1, HIGH);    
-  digitalWrite(Groc1, LOW);   
-  digitalWrite(Verd1, LOW);    
-  
-  digitalWrite(Vermell2, HIGH);    
-  digitalWrite(Groc2, LOW);    
-  digitalWrite(Verd2, LOW);    
+  digitalWrite(A, HIGH);    
+  digitalWrite(B, LOW);   
+  digitalWrite(C, HIGH);    
+  digitalWrite(D, LOW);    
+  digitalWrite(E, HIGH);    
+  digitalWrite(F, LOW);   
+  digitalWrite(G, LOW);   
 
   delay (temps);
 
-  digitalWrite(Vermell1, HIGH);    
-  digitalWrite(Groc1, LOW);   
-  digitalWrite(Verd1, LOW);    
-  
-  digitalWrite(Vermell2, LOW);    
-  digitalWrite(Groc2, LOW);    
-  digitalWrite(Verd2, HIGH);    
+  digitalWrite(A, HIGH);    
+  digitalWrite(B, HIGH);   
+  digitalWrite(C, HIGH);    
+  digitalWrite(D, LOW);    
+  digitalWrite(E, HIGH);    
+  digitalWrite(F, HIGH);   
+  digitalWrite(G, HIGH);   
 
   delay (temps);
 
-  digitalWrite(Vermell1, HIGH);    
-  digitalWrite(Groc1, LOW);   
-  digitalWrite(Verd1, LOW);    
-  
-  digitalWrite(Vermell2, LOW);    
-  digitalWrite(Groc2, LOW);    
-  digitalWrite(Verd2, HIGH);    
+  digitalWrite(A, LOW);    
+  digitalWrite(B, HIGH);   
+  digitalWrite(C, HIGH);    
+  digitalWrite(D, LOW);    
+  digitalWrite(E, HIGH);    
+  digitalWrite(F, HIGH);   
+  digitalWrite(G, HIGH);   
 
   delay (temps);
 
-  digitalWrite(Vermell1, HIGH);    
-  digitalWrite(Groc1, LOW);   
-  digitalWrite(Verd1, LOW);    
-  
-  digitalWrite(Vermell2, LOW);    
-  digitalWrite(Groc2, LOW);    
-  digitalWrite(Verd2, HIGH);    
+  digitalWrite(A, LOW);    
+  digitalWrite(B, LOW);   
+  digitalWrite(C, LOW);    
+  digitalWrite(D, LOW);    
+  digitalWrite(E, LOW);    
+  digitalWrite(F, LOW);   
+  digitalWrite(G, LOW);  
 
   delay (temps);
 
-  digitalWrite(Vermell1, HIGH);    
-  digitalWrite(Groc1, LOW);   
-  digitalWrite(Verd1, LOW);    
-  
-  digitalWrite(Vermell2, LOW);    
-  digitalWrite(Groc2, LOW);    
-  digitalWrite(Verd2, HIGH);    
+  digitalWrite(A, LOW);    
+  digitalWrite(B, LOW);   
+  digitalWrite(C, HIGH);    
+  digitalWrite(D, HIGH);    
+  digitalWrite(E, HIGH);    
+  digitalWrite(F, HIGH);   
+  digitalWrite(G, HIGH);  
 
   delay (temps);
 
-  digitalWrite(Vermell1, HIGH);    
-  digitalWrite(Groc1, LOW);   
-  digitalWrite(Verd1, LOW);    
-  
-  digitalWrite(Vermell2, LOW);    
-  digitalWrite(Groc2, HIGH);    
-  digitalWrite(Verd2, LOW);    
+  digitalWrite(A, HIGH);    
+  digitalWrite(B, LOW);   
+  digitalWrite(C, LOW);    
+  digitalWrite(D, HIGH);    
+  digitalWrite(E, HIGH);    
+  digitalWrite(F, HIGH);   
+  digitalWrite(G, HIGH);  
 
   delay (temps);
 
-  digitalWrite(Vermell1, HIGH);    
-  digitalWrite(Groc1, LOW);   
-  digitalWrite(Verd1, LOW);    
-  
-  digitalWrite(Vermell2, HIGH);    
-  digitalWrite(Groc2, LOW);    
-  digitalWrite(Verd2, LOW); 
+  digitalWrite(A, HIGH);    
+  digitalWrite(B, LOW);   
+  digitalWrite(C, HIGH);    
+  digitalWrite(D, HIGH);    
+  digitalWrite(E, LOW);    
+  digitalWrite(F, HIGH);   
+  digitalWrite(G, HIGH);  
 
   delay (temps);
 
-  digitalWrite(Vermell1, LOW);    
-  digitalWrite(Groc1, LOW);   
-  digitalWrite(Verd1, HIGH);    
-  
-  digitalWrite(Vermell2, HIGH);    
-  digitalWrite(Groc2, LOW);    
-  digitalWrite(Verd2, LOW); 
+  digitalWrite(A, HIGH);    
+  digitalWrite(B, HIGH);   
+  digitalWrite(C, HIGH);    
+  digitalWrite(D, HIGH);    
+  digitalWrite(E, HIGH);    
+  digitalWrite(F, HIGH);   
+  digitalWrite(G, LOW);  
 
   delay (temps);
 
-  digitalWrite(Vermell1, LOW);    
-  digitalWrite(Groc1, LOW);   
-  digitalWrite(Verd1, HIGH);    
-  
-  digitalWrite(Vermell2, HIGH);    
-  digitalWrite(Groc2, LOW);    
-  digitalWrite(Verd2, LOW); 
+  digitalWrite(A, LOW);    
+  digitalWrite(B, LOW);   
+  digitalWrite(C, LOW);    
+  digitalWrite(D, LOW);    
+  digitalWrite(E, HIGH);    
+  digitalWrite(F, LOW);   
+  digitalWrite(G, HIGH);  
 
   delay (temps);
 
-  digitalWrite(Vermell1, LOW);    
-  digitalWrite(Groc1, LOW);   
-  digitalWrite(Verd1, HIGH);    
-  
-  digitalWrite(Vermell2, HIGH);    
-  digitalWrite(Groc2, LOW);    
-  digitalWrite(Verd2, LOW); 
+  digitalWrite(A, HIGH);    
+  digitalWrite(B, HIGH);   
+  digitalWrite(C, HIGH);    
+  digitalWrite(D, LOW);    
+  digitalWrite(E, HIGH);    
+  digitalWrite(F, HIGH);   
+  digitalWrite(G, HIGH); 
 
   delay (temps);
 
-  digitalWrite(Vermell1, LOW);    
-  digitalWrite(Groc1, LOW);   
-  digitalWrite(Verd1, HIGH);    
-  
-  digitalWrite(Vermell2, HIGH);    
-  digitalWrite(Groc2, LOW);    
-  digitalWrite(Verd2, LOW); 
-
-  delay (temps);
-
-  digitalWrite(Vermell1, LOW);    
-  digitalWrite(Groc1, HIGH);   
-  digitalWrite(Verd1, LOW);    
-  
-  digitalWrite(Vermell2, HIGH);    
-  digitalWrite(Groc2, LOW);    
-  digitalWrite(Verd2, LOW); 
-
-  delay (temps);
-
-  digitalWrite(Vermell1, HIGH);    
-  digitalWrite(Groc1, LOW);   
-  digitalWrite(Verd1, LOW);    
-  
-  digitalWrite(Vermell2, HIGH);    
-  digitalWrite(Groc2, LOW);    
-  digitalWrite(Verd2, LOW); 
-
-  delay (temps);
 }
